66349309894e69e4c7daf2c769154b369c1e5d0b
LuizFernando11 <luiz.fernando@viannasempre.com.br>
2024-10-15 22:52:35 -0300

    Iniciando projeto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Luiz Fernando Duarte Cabette</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="shortcut icon" type="image/png " href="{{ asset('images/logo.png') }}"
+</head>
+<body>
+    
+</body>
+</html>
