c61a64b8cbb635a81f452dc5e75c82cd1dfff7e4
LuizFernando11 <luiz.fernando@viannasempre.com.br>
2024-10-17 22:02:23 -0300

    subindo repositorio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Luiz Fernando Duarte Cabette</title>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    <link rel="shortcut icon" type="image/png " href="{{ asset('images/logo.png') }}"
+    <link rel="shortcut icon" type="image/png " href="{{ asset('images/logo.png') }}">
 </head>
 <body>
+
+<header class="container">
+    <div class="navbar">
+        <div class="logo">
+            <a href=""><img src="{{ asset('images/logo.png') }}" alt=""></a>
+        </div>
+        <div class="menu-opener">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <nav>
+        <a href="">Blog</a>
+        <a href="">Contato</a>
+    </nav>
+</header>
+
     
 </body>
 </html>
